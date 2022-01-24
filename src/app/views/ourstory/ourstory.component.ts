7be7b1a19813ec3faa19efdc4a83001028c62af6
Clinton Mutinda <mutinda1993@gmail.com>
2022-01-24 17:56:56 +0300

    Our Story Timeline
    Added functionality of clicking on scroll on the Our Story page

--- src/app/views/ourstory/ourstory.component.ts
+++ src/app/views/ourstory/ourstory.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import $ from 'jquery';
 
 @Component({
   selector: 'app-ourstory',
@@ -10,6 +11,62 @@ export class OurstoryComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  openTab(event){
+
+    let currentLink = event.target;
+
+    $(".active").removeClass('active');
+    $(currentLink).addClass('active');
+
+  }
+
+  changeImage(event){
+
+    let currentImage = event.target.dataset.background;
+
+    $(".backgroundImage").hide();
+    $('.' + currentImage).show();
+
+    console.log(currentImage);
+    
+  }
+
+  showTimeline(event){
+
+    let currentTimeline = event.target.dataset.timeline;
+
+    $(".timelineSidebar").show();
+    
+    console.log(currentTimeline);
+
+  }
+
+  hideTimeline(){
+    
+    $(".timelineSidebar").hide();
+  }
+
+  openTimeLineTab(event){
+
+    let currentTimelineLink = event.target;
+    
+    $(".active").removeClass('active');
+    $(currentTimelineLink).addClass('active');
+
+    $("nav ul .timelineLink .active").remove('active');
+
+  }
+
+  scrollPage(event){
+
+    let targetDiv = event.target.dataset.target;
+    let contentDiv = $('.' + targetDiv);
+
+    $('html, body').stop().animate({
+      scrollTop: contentDiv.offset().top
+    }, 400);
   }
 
 }
