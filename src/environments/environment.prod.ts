f3e4c215af170ec9def94bdb89a1de9f6d944984
John AKello <jakello@mfs.co.ke>
2022-01-31 14:25:25 +0300

    Env production updated with endpoints

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  contentRoot:"http://10.38.84.142/test-with-postgres/index.php/wp-json/wp/v2/",
+  contactEndPoint:"http://10.38.84.142/test-with-postgres/index.php/wp-json/contact-us/v1/send",
 };
