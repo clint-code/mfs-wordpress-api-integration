01146eaf0e2b85438b03885cc1d5e8e67569cbf0
John AKello <jakello@mfs.co.ke>
2022-01-31 16:24:33 +0300

    Environment value changed to webapp.cole.mfs

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  contentRoot:"http://10.38.84.142/test-with-postgres/index.php/wp-json/wp/v2/",
-  contactEndPoint:"http://10.38.84.142/test-with-postgres/index.php/wp-json/contact-us/v1/send",
+  contentRoot:"http://webapp.colo.mfs.co.ke/test-with-postgres/index.php/wp-json/wp/v2/",
+  contactEndPoint:"http://webapp.colo.mfs.co.ke/test-with-postgres/index.php/wp-json/contact-us/v1/send",
 };
 
 /*
