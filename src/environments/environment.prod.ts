d4045be8fce2fd1a5b4f8d789984f1b732821b1c
John AKello <jakello@mfs.co.ke>
2022-02-02 14:31:52 +0300

    Env value updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  contentRoot:"https://website.mfs.co.ke/wp-json/wp/v2/",
-  contactEndPoint:"https://website.mfs.co.ke/wp-json/contact-us/v1/send",
+  contentRoot:" https://website-bcnd.mfs.co.ke/wp-json/wp/v2/",
+  contactEndPoint:" https://website-bcnd.mfs.co.ke//wp-json/contact-us/v1/send",
 };
